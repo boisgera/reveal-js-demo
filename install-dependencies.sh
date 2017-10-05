b77986365861bae0220ca2b89501f8a45f54da1e
Sébastien Boisgérault <Sebastien.Boisgerault@gmail.com>
2017-10-05 16:35:55 +0200

    test pandoc install

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -6,4 +6,5 @@ curl $MINICONDA_INSTALLER > miniconda.sh
 bash miniconda.sh -b -p $HOME/miniconda
 export PATH="$HOME/miniconda/bin:$PATH"
 conda install --yes -c conda-forge pandoc=1.19.2
+pandoc --version
 
